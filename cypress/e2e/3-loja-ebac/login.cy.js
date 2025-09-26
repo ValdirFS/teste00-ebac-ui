2a976714b9e19ba891459d960b93ae057232accf
Valdir Félix <vfelixjs@gmail.com>
2025-09-26 19:00:35 -0300

    testes negativos e hooks

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -2,13 +2,35 @@
 
 describe('Funcionalidade: Login' ,() => {
 
-     it('Deve fazer login com sucesso' ,() => {
-         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+
+    });
+    
+    it('Deve fazer login com sucesso' ,() => {
          cy.get('#username').type('valdir.teste@100.com.br')
          cy.get('#password').type('teste@100')
          cy.get('.woocommerce-form > .button').click()
-
          cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, valdir.teste-5154 (não é valdir.teste-5154? Sair)')
      })
+
+    it('Deve exibir uma messagem de erro ao inserir usuário inválido ', () => {
+         cy.get('#username').type('valdir@100.com.br')
+         cy.get('#password').type('teste@100')
+         cy.get('.woocommerce-form > .button').click()
+         cy.get('.woocommerce-error').should('exist')
+    });
+
+    it('Deve exibir uma messagem de erro ao inserir senha inválida', () => {
+         cy.get('#username').type('valdir.teste@100.com.br')
+         cy.get('#password').type('teste@000')
+         cy.get('.woocommerce-form > .button').click()
+         cy.get('.woocommerce-error').should('contain' , 'Erro: A senha fornecida para o e-mail valdir.teste@100.com.br está incorreta. Perdeu a senha?')
+         cy.get('.woocommerce-error').should('exist')
+   });
 
 })
