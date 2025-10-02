cea16dce4cf368b44adbacdee315e48bf75d2e4f
Valdir Félix <vfelixjs@gmail.com>
2025-10-02 12:08:26 -0300

    arq de dados

--- cypress/e2e/3-loja-ebac/cadastro.cy.js
+++ cypress/e2e/3-loja-ebac/cadastro.cy.js
@@ -4,7 +4,7 @@ import { faker } from '@faker-js/faker';
 describe('Funcionalidade: Cadasto' , () => {
       
     beforeEach( () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
 
     });
 
---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Produtos' , () => {
+
+    beforeEach(() => {
+        cy.visit('produtos')
+    });
+    
+    
+    it('Deve selecionar um produto de lista', () => {
+        cy.get('.product-block ')
+             //.first()
+             //.last()
+             //.eq(2)
+             .contains('Apollo Running Short')
+             .click()
+
+             cy.get('#tab-title-description > a').should('contain' , 'Descrição')
+        
+    });
+});
